4f6794845afd3a9cd223e7872f320b5a3c02eae8
ZararB <zarar.learner@gmail.com>
2020-07-20 00:50:10 -0400

    Save

--- __main__.py
+++ __main__.py
@@ -1,16 +1,12 @@
-#import videoNoob
-#import youtubeHelper
 
-#vidNoob = videoNoob.VideoNoob()
-#yth = youtubeHelper.youtubeHelper()
-
-#ttk.get_trending(50)
+from Agent import Agent
 
 
-#vidFilePath = vidNoob.create_video()
-#vidFilePath = "data/videos/firstvid.mp4"
-#yth.upload_video("quickdopehit", vidFilePath, "HelloWorld", "Muhahaahha")
 
-print('Hello from beyond')
-import sys
-print(sys.version)
+
+if __name__ == '__main__':
+
+    agent = Agent()
+
+    agent.update()
+    
