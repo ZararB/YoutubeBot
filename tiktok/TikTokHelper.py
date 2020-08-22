26dc5780a8c267fe97e7a91fe29a0a5920b8dd34
naymat <naymat30@gmail.com>
2020-08-22 15:43:08 -0400

    Put all models to database/models module; refactored connection information to connection.py

--- tiktok/TikTokHelper.py
+++ tiktok/TikTokHelper.py
@@ -1,22 +1,16 @@
 from TikTokApi import TikTokApi
 from pathlib import Path
-import os 
-from tiktok.tiktok import Tiktok
-from tiktok.tiktokChannel import TiktokChannel
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine('postgresql://postgres:wabbalabba@localhost/youtubebot', echo=True)
-Session = sessionmaker(bind = engine)
+import os
+from database.models.tiktok import Tiktok
+from database.models.tiktokChannel import TiktokChannel
+from database.connection import Session
 
 
 class TikTokHelper():
 
-
     def __init__(self):
         self.api = TikTokApi()
         self.session = Session()
-
 
     def getTrending(self, count=10):
 
@@ -26,7 +20,6 @@ class TikTokHelper():
             nickname = tiktokDict['author']['nickname']
             channelUniqueId = tiktokDict['author']['uniqueId']
             tiktokId = tiktokDict['id']
-            
 
             channelPath = 'data/tiktok/' + channelUniqueId
             if not os.path.isdir(channelPath):
@@ -40,7 +33,7 @@ class TikTokHelper():
                     pass
 
             downloadLocation = channelPath + '/' + tiktokId + '.mp4'
-            
+
             if not os.path.isfile(downloadLocation):
                 # Implement logging
                 vid_data = self.api.get_Video_By_TikTok(tiktokDict)
@@ -50,16 +43,13 @@ class TikTokHelper():
                 tiktok = Tiktok.tiktokFromTiktokDict(tiktokDict, downloadLocation)
                 self.session.add(tiktok)
 
-
             self.session.commit()
-
 
     def getChannelTiktoks(self, channelUniqueId, count=50):
 
         print('Getting user {}...'.format(channelUniqueId))
         channelPath = 'data/tiktok/' + channelUniqueId
 
-        
         if not os.path.isdir(channelPath):
             Path(channelPath).mkdir(exist_ok=True)
             try:
@@ -70,7 +60,6 @@ class TikTokHelper():
             except:
                 pass
 
-
         tiktoks = []
 
         try:
@@ -78,10 +67,9 @@ class TikTokHelper():
         except Exception:
             print(Exception)
 
-
         for tiktokDict in tiktoks:
             downloadLocation = channelPath + '/' + tiktokDict['id'] + '.mp4'
-            
+
             # Skip if already downloaded
 
             if not os.path.isfile(downloadLocation):
@@ -93,8 +81,6 @@ class TikTokHelper():
                 self.session.add(tiktok)
             else:
                 # Log or print that tiktok is already downloaded 
-                pass 
+                pass
 
         self.session.commit()
-
-    
