ec59c365a5cde37f5d0eedba366e6e58a192fd14
naymat <naymat30@gmail.com>
2020-08-27 20:14:10 -0400

    added database methods for youtube api and removed the old youtube_api location

--- debugging.py
+++ debugging.py
@@ -1,6 +1,7 @@
-from tiktok import TikTokHelper
+from youtube.youtube_api.scraper_api import search_youtube
+from youtube.youtube_api.scraper_api import scrape_channel
+from database.youtube import get_unscraped_channel_urls
 
 if __name__ == '__main__':
-    helper = TikTokHelper.TikTokHelper()
-
-    helper.getTrending(10)
+    scrape_channel(get_unscraped_channel_urls(limit=1))
+    print(get_unscraped_channel_urls(limit=1))
